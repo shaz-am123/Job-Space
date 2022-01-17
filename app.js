c69cc885be5e1daef9c6a839c1c0c826afaa2563
nikhil vyas <vyasn0123@gmail.com>
2022-01-17 19:22:55 +0530

    Job seeker table altered, Queries added

--- app.js
+++ app.js
@@ -131,7 +131,7 @@ app.get("/job_listing",(req,res)=>{
     {
         myConnection.query('select job_post.job_profile, job_post.job_description, job_post.apply_by, job_post.location, job_post.salary, Company.C_name from job_post inner join Company on job_post.j_id = Company.c_id', (err, rows, fields)=>{
         if(!err){
-          console.log(rows);
+          //console.log(rows);
           res.render("joblisting", {
             rows: rows
           });
@@ -147,14 +147,14 @@ app.get("/job_listing",(req,res)=>{
 
 app.post("/job_listing", (req,res)=>{
   if(user.isAuthenticated){
-    console.log(req.body.searchInput);
+    console.log(req.body);
     var sqlQuery = 'select job_post.job_profile, job_post.job_description, job_post.apply_by, job_post.location, job_post.salary, Company.C_name from job_post inner join Company on job_post.j_id = Company.c_id where job_post.job_profile = "' + req.body.searchInput + '" OR job_post.job_description = "' + req.body.searchInput + '" OR job_post.location = "' + req.body.searchInput + '" OR job_post.salary = "' + req.body.searchInput + '" OR Company.C_name = "' + req.body.searchInput + '" ;';
     if( ((req.body.searchInput === undefined) || (req.body.searchInput === '')) ){
       res.redirect("/job_listing");
     }else{
       myConnection.query(sqlQuery, function(err, rows, fields){
         if(!err){
-          console.log(rows);
+          //console.log(rows);
           res.render("joblisting", {
             rows: rows
           });
@@ -164,6 +164,26 @@ app.post("/job_listing", (req,res)=>{
         }
       });
     }
+    //console.log(JSON.parse(req.body.applybutton));
+    var sqlQueryInsert = 'select j_id, c_id from job_post where job_post.job_profile = "' + JSON.parse(req.body.applybutton).job_profile + '" AND job_post.job_description="' + JSON.parse(req.body.applybutton).job_description + '" AND job_post.location="' + JSON.parse(req.body.applybutton).location + '" AND job_post.salary="' + JSON.parse(req.body.applybutton).salary + '"; '
+    //console.log(sqlQueryInsert);
+    myConnection.query(sqlQueryInsert, function(err,rows,fields){
+      if(!err){
+        //console.log(rows);
+        //console.log(user.userEmail);
+        var sqlQueryInApply = 'insert into applied values ("' + user.userEmail + '", ' + rows[0].j_id + ', ' + rows[0].c_id + '); ';
+        //console.log(sqlQueryInApply);
+        myConnection.query(sqlQueryInApply, function(err, rows, fields){
+          if(!err){
+            console.log("Successfully inserted into applied tables");
+          }else{
+            console.log(err);
+          }
+        })
+      }else{
+        console.log(err);
+      }
+    })
   }
   else{
     res.send("</h1>NOT SIGNNED IN</h1>");
@@ -214,6 +234,60 @@ app.post("/StudentForm",(req,res)=>{
         console.log("Data inserted into skill_set table");
       }
     });
+
+    const sqlQuery4 = "SELECT ed_id from education where degree = '"+req.body.degree+"' AND major = '"+req.body.major+"' AND university = '"+req.body.university+"' AND cgpa = '"+req.body.cgpa+"' AND percent =  '"+req.body.percentage+"' AND  batch = '"+req.body.batch+"';";
+    const sqlQuery5 = "SELECT exp_id from Experience WHERE current_job = '"+req.body.curr_job+"' AND  company_name = '"+req.body.comp_name+"' AND start_date = '"+req.body.start_d+"' AND  end_date = '"+req.body.end_d+"' AND location =  '"+req.body.loc+"';";
+    const sqlQuery6 = "SELECT skill_id from skill_set WHERE skill_set_name = '"+req.body.skill+"' AND  cv = '"+req.body.cv+"';";
+
+    myConnection.query(sqlQuery4, function(err, result){
+      if(!err){
+        console.log(result);
+        sqlQuery7 = "update job_seeker_profile set ed_id  = "  + result[0].ed_id +  " where email = '" + user.userEmail + "';";
+        console.log(sqlQuery7);
+        myConnection.query(sqlQuery7, function(err, rows){
+          if(!err){
+            console.log("Inserted ed_id in job_seeker_profile");
+          }else{
+            console.log(err);
+          }
+        })
+      }else{
+        console.log("error occurred");
+      }
+    })
+    myConnection.query(sqlQuery5, function(err, result){
+      if(!err){
+        console.log(result);
+        sqlQuery8 = "update job_seeker_profile set exp_id  = "  + result[0].exp_id +  " where email = '" + user.userEmail + "';";
+        console.log(sqlQuery8);
+        myConnection.query(sqlQuery8, function(err, rows){
+          if(!err){
+            console.log("Inserted exp_id in job_seeker_profile");
+          }else{
+            console.log(err);
+          }
+        })
+      }else{
+        console.log("error occurred");
+      }
+    })
+    myConnection.query(sqlQuery6, function(err, result){
+      if(!err){
+        console.log(result);
+        sqlQuery9 = 'update job_seeker_profile set skill_id  = '  + result[0].skill_id +  ' where email = "' + user.userEmail + '";';
+        console.log(sqlQuery9);
+        myConnection.query(sqlQuery9, function(err, rows){
+          if(!err){
+            console.log("Inserted skill_id in job_seeker_profile");
+          }else{
+            console.log(err);
+          }
+        })
+      }else{
+        console.log("error occurred");
+      }
+    })
+
 
     if(!error)
       res.redirect("/job_listing");
@@ -308,7 +382,7 @@ app.post("/profile_listing", (req,res)=>{
 
   console.log(req.body.searchInput);
 
-  const sqlQuery2 = 'SELECT * from job_seeker_profile JOIN Experience ON job_seeker_profile.exp_id = Experience.exp_id JOIN education ON job_seeker_profile.ed_id = education.ed_id JOIN skill_set ON job_seeker_profile.skill_id = skill_set.skill_id WHERE Experience.current_job = "' + req.body.searchInput + '" OR Experience.company_name="' + req.body.searchInput + '" OR Experience.location = "JSSSTU" OR education.degree = "' + req.body.searchInput + '" OR education.major="' + req.body.searchInput + '" OR education.university="' + req.body.searchInput + '" OR education.cgpa="' + req.body.searchInput + '" OR education.percent="' + req.body.searchInput + '" OR education.batch="' + req.body.searchInput + '";';
+  const sqlQuery2 = 'SELECT * from job_seeker_profile JOIN Experience ON job_seeker_profile.exp_id = Experience.exp_id JOIN education ON job_seeker_profile.ed_id = education.ed_id JOIN skill_set ON job_seeker_profile.skill_id = skill_set.skill_id JOIN applied ON job_seeker_profile.email = applied.email WHERE Experience.current_job = "' + req.body.searchInput + '" OR Experience.company_name="' + req.body.searchInput + '" OR Experience.location = "JSSSTU" OR education.degree = "' + req.body.searchInput + '" OR education.major="' + req.body.searchInput + '" OR education.university="' + req.body.searchInput + '" OR education.cgpa="' + req.body.searchInput + '" OR education.percent="' + req.body.searchInput + '" OR education.batch="' + req.body.searchInput + '";';
 
   console.log(sqlQuery2);
 
